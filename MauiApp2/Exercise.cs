fe0e2c4a4b76bcff65dd58f3be13b6b27fe60b67
lxdi1882 <lxdi1883@gmail.com>
2026-07-19 23:28:17 +0800

    Initial commit

---
+++ MauiApp2/Exercise.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiApp2;
+
+public class Exercise
+{
+    public string Name { get; set; }
+    public string MuscleGroup { get; set; }
+    public string SubTarget { get; set; }
+}
---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+    private int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private void OnAddExerciseClicked(object? sender, EventArgs e)
+    {
+        var exerciseName = ExerciseInput.Text;
+
+        var exerciseLabel = new Label { Text = $"Exercise: {exerciseName}" };
+        var logButton = new Button { Text = $"Log : {exerciseName}" };
+
+        logButton.Clicked += (s, args) =>
+        {
+            exerciseLabel.Text = $"{exerciseName} - logged!";
+            logButton.IsEnabled = false;
+        };
+
+        MainStack.Children.Add(exerciseLabel);
+        MainStack.Children.Add(logButton);
+
+        ExerciseInput.Text = "";
+    }
+}
