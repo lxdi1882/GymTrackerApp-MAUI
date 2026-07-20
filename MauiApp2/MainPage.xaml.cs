655a02e33437357225d60b0738ae4352c0457e7b
lxdi1882 <lxdi1883@gmail.com>
2026-07-20 18:38:01 +0800

    added working set logger with per excercise tracking via dictionary

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -29,4 +29,53 @@ public partial class MainPage : ContentPage
     //
     //     ExerciseInput.Text = "";
     // }
+    
+    private Dictionary<string, ExerciseEntry> workoutInProgress = new Dictionary<string, ExerciseEntry>();
+
+    private void OnAddSetClicked(object sender, EventArgs e)
+    {
+        string selectedName = ExercisePicker.SelectedItem?.ToString();
+        if (selectedName == null)
+        {
+            SetsLabel.Text = " please select an exercise first.";
+            return;
+        }
+
+        double weight = double.Parse(WeightInput.Text);
+        int reps = int.Parse(RepsInput.Text);
+        
+        var newSet = new SetEntry{Weight = weight, Reps = reps};
+        
+        if (!workoutInProgress.ContainsKey(selectedName))
+        {
+            var exercise = ExerciseLibrary.All.First(ex => ex.Name == selectedName);
+            workoutInProgress[selectedName] = new ExerciseEntry
+            {
+                Exercise = exercise,
+                Sets = new List<SetEntry>()
+            };
+        }
+
+        workoutInProgress[selectedName].Sets.Add(newSet);
+
+        // rebuild the display from everything tracked so far
+        string summary = "";
+        foreach (var entry in workoutInProgress.Values)
+        {
+            summary += $"\n{entry.Exercise.Name} ({entry.Exercise.MuscleGroup}):\n";
+            foreach (var set in entry.Sets)
+            {
+                summary += $"  {set.Weight}kg x {set.Reps} reps\n ";
+            }
+        }
+        SetsLabel.Text = summary;
+
+        WeightInput.Text = "";
+        RepsInput.Text = "";
+    
+    }
+    
+        
+
+    
 }
